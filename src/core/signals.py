42f64860d41a3aebf4884640fef447c6540b0329
Programming Bear <pshkmr007@gmail.com>
2021-01-11 05:13:06 +0000

    allset

--- src/core/signals.py
+++
@@ -1,7 +0,0 @@
-from django.dispatch import Signal
-
-pizza_done = Signal(providing_args=["toppings", "size"])
-
-class Pizza:
-    def mark_as_done(self, toppings, size):
-        pizza_done.send(sender=self.__class__, toppings=toppings, size=size)
